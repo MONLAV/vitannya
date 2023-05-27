4e829cda33db3a9c13686a3e330958236f2a80eb
MONLAV <127471489+MONLAV@users.noreply.github.com>
2023-05-27 13:16:51 +0300

    come beak

--- slider.js
+++ slider.js
@@ -9,14 +9,14 @@ const swiper = new Swiper('.swiper', {
   },
 
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 });
 
